1d265ca48ce1de0e5edfb427a300126f411a397a
Milosz Szymanski <milosz.jan.szymanski@gmail.com>
2024-02-06 15:00:24 +0100

    Added pom.xml and code for 2 automated tests

---
+++ src/test/java/org/example/PajacykAutomation.java
@@ -0,0 +1,43 @@
+
+package org.example;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+public class PajacykAutomation{
+    WebDriver driver;
+    String mainPage = "https://www.pajacyk.pl/";
+
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void quitDriver(){
+        driver.quit();
+    }
+
+    @Test
+    public void checkTitle() {
+        driver.get(mainPage);
+        String mainPageExpectedTitle = "Pajacyk – Pajacyk od wielu lat dożywia dzieci. Pamiętaj, że kliknięcie w brzuszek, to pierwszy krok, by pomóc dzieciom.";
+        String mainPageActualTitle = driver.getTitle();
+        Assertions.assertEquals(mainPageExpectedTitle,mainPageActualTitle,"Actual title is different than expected title");
+    }
+
+    @Test
+    public void checkClickPajacyk() {
+        driver.get(mainPage);
+        WebElement donateButton = driver.findElement(By.className("pajacyk__clickbox"));
+        donateButton.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        WebElement element = driver.findElement(By.className("pajacyk__thankyou"));
+        WebElement p1Element = element.findElement(By.className("p2"));
+        Assertions.assertEquals("dziękujemy :)", p1Element.getText());
+
+}}
