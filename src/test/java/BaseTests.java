2e2daf2924a6c11014f70d184336b4dea5a6aadc
Milosz Szymanski <milosz.jan.szymanski@gmail.com>
2024-08-21 20:07:19 +0200

    new test, added code for wait.until(ExpectedConditions....)

--- src/test/java/BaseTests.java
+++ src/test/java/BaseTests.java
@@ -4,24 +4,34 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 public class BaseTests {
     protected WebDriver driver;
     protected ActionBot bot;
-
+    WebDriverWait wait;
     @BeforeEach
     public void setup() {
+
         ChromeOptions options = new ChromeOptions();
-
-        options.addArguments("--disable-search-engine-choice-screen");
-
         driver = new ChromeDriver(options);
+        options.addArguments("--disable-search-engine-choice-screen");
+        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+
+
+
+
+
         String baseURL = "https://www.pajacyk.pl";
         bot = new ActionBot(driver, baseURL);
+
     }
 
     @AfterEach
     public void quitDriver() {
         driver.quit();
     }
+
 }
--- src/test/java/MainPageTests.java
+++ src/test/java/MainPageTests.java
@@ -3,52 +3,47 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import java.io.*;
-import java.lang.Thread;
 import java.time.Duration;
-
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
 public class MainPageTests extends BaseTests {
 
-
+    String mainPageExpectedTitle = "Pajacyk – Pajacyk od wielu lat dożywia dzieci. Pamiętaj, że kliknięcie w brzuszek, to pierwszy krok, by pomóc dzieciom.";
+    String thankYouClass = "pajacyk__thankyou";
+    String divergentTitle = "Actual title is different than expected title";
+    String thankYouXpath = ".//section[contains(@class, 'pajacyk__thankyou')]//p[2]";
+    String thankYouText = "Dziękujemy!!!";
 
     @Test
     @DisplayName("Verifies that main page is displayed - by title")
     public void checkTitle() {
         bot.go();
-        String mainPageExpectedTitle = "Pajacyk – Pajacyk od wielu lat dożywia dzieci. Pamiętaj, że kliknięcie w brzuszek, to pierwszy krok, by pomóc dzieciom.";
-        String title = driver.getTitle();
-        Assertions.assertEquals(mainPageExpectedTitle, title, "Actual title is different than expected title");
-    }
-    @Test
-    @DisplayName("Verifies that after clicking on toy clown proper message is displayed")
-    public void checkClickPajacyk() {
-        bot.go();
-        WebElement donateButton = driver.findElement(By.className("pajacyk__clickbox"));
-        donateButton.click();
-        try{
-            Thread.sleep(900000);
-        }
-        catch (Exception e) {
 
-            // catching the exception
-            System.out.println(e);
-        }
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
-        WebElement element = driver.findElement(By.className("pajacyk__thankyou"));
-        WebElement p1Element = element.findElement(By.className("p2"));
-        Assertions.assertEquals("dziękujemy :)", p1Element.getText());
+        String title = driver.getTitle();
+        Assertions.assertEquals(mainPageExpectedTitle, title, divergentTitle);
     }
+
     @Test
     @DisplayName("Verifies that after clicking on toy clown proper message is displayed")
     public void checkClickPajacykTest() {
         bot.go();
-        WebElement donateButton = driver.findElement(By.className("pajacyk__clickbox"));
+        WebElement donateButton = driver.findElement(By.className(thankYouClass));
         donateButton.click();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
-        WebElement element = driver.findElement(By.className("pajacyk__thankyou"));
-        WebElement p1Element = element.findElement(By.className("p2"));
-        Assertions.assertEquals("dziękujemy :)", p1Element.getText());
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(thankYouClass)));
+        WebElement thankYouLocator = driver.findElement(By.xpath(thankYouXpath));
+        System.out.println(thankYouLocator.getText());
+        Assertions.assertEquals("Dziękujemy!!!", thankYouLocator.getText());
+    }
+    @Test
+    @DisplayName("Verifies that after clicking on round button 'Wesprzyj' and on the bottom proper page is displayed")
+    public void checkExternalLinks() {
+        bot.go();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        final WebElement contributeButton = driver.findElement(By.className("buttons__item--pink"));
+        contributeButton.click();
+        String expectedContributePageUrl = "https://www.pajacyk.pl/wesprzyj/";
+        String actualContributePageUrl = driver.getCurrentUrl();
+        Assertions.assertEquals(expectedContributePageUrl, actualContributePageUrl, "Actual contribute page is different than expected");
     }
 
 }
